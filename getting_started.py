c035fe522009b2f567169bcb5cca6561d6f11702
Rabbidon <edwinfennell1@gmail.com>
2019-12-02 15:27:07 +0000

    Added more basic tensor properties to getting_started

--- getting_started.py
+++ getting_started.py
@@ -9,5 +9,19 @@ print(torch.rand(5,3))
 # Initialise matrix with zeros, and enforce type long
 print(torch.zeros(5, 3, dtype=torch.long))
 
-# Construct tensor directly from data
-print([5.5,3])
+# Construct tensor directly from data (also convert existing arrays to tensors)
+
+x=torch.tensor([5.5,3])
+print(x)
+
+# COnstruct new tensor with properties of given tensorN
+
+x = x.new_ones(5, 3, dtype=torch.double)
+print(x)
+
+x = x.new_ones(5, 3, dtype=torch.float)
+print(x) 
+
+# Convert tensorflow tensor to numpy array
+
+print(x.numpy())
